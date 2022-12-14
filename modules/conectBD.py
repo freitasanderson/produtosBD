35e246738fdbfc96f80866bc9bebfd4de80b4cfa
Anderson Freitas <108918987+freitasanderson@users.noreply.github.com>
2022-12-14 00:53:01 -0300

    Update conectBD.py

--- modules/conectBD.py
+++ modules/conectBD.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 #conexão com banco de dados
 def Conectar():
-    con = mysql.connector.connect(host='teste.c0sjio5nc9zh.sa-east-1.rds.amazonaws.com',database='bd_produtos',user='root',password='andex301')
+    con = mysql.connector.connect(host='teste.c0sjio5nc9zh.sa-east-1.rds.amazonaws.com',database='bd_produtos',user='root',password='123456789a')
     return con
 
     
